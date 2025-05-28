ae1296de2f6f0281f876f47829578b489ca5762f
Marvin Barrera <59856529+MarvinBT1988@users.noreply.github.com>
2025-05-28 03:10:28 +0000

    update cambios

--- src/main/java/com/example/demo/controladores/HomeController.java
+++ src/main/java/com/example/demo/controladores/HomeController.java
@@ -6,12 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    // @GetMapping("/") mapea las solicitudes HTTP GET a la ruta raíz ("/").
-    // Cuando un cliente accede a la raíz, Spring Boot buscará automáticamente un archivo
-    // llamado "index.html" en la carpeta 'static' (o 'public', 'resources').
-    // Por lo tanto, no necesitamos un método que devuelva una cadena para el "Hola Mundo"
-    // si queremos que el HTML sea la página principal.
-    // Sin embargo, si quisieras una ruta específica para el HTML, podrías hacer:
+   
     @GetMapping("/")
     public String inicio() {
         return "index.html"; // Esto devolvería el nombre del archivo HTML
